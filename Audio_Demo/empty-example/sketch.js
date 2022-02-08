f40ab1f9cbe8a21e50f87f0a52e3b2ea1741bf09
MrWPrice <76269054+MrWPrice@users.noreply.github.com>
2022-02-08 17:02:26 +0000

    more

--- Audio_Demo/empty-example/sketch.js
+++ Audio_Demo/empty-example/sketch.js
@@ -1,7 +1,23 @@
-function setup() {
-  // put setup code here
+let mic;
+
+ function setup(){
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+  background(0);
+  fill(255);
+  text('tap to start', width/2, 20);
+
+  micLevel = mic.getLevel();
+  let y = height - micLevel * height;
+  ellipse(width/2, y, 10, 10);
 }
+buttons
+copy
+reset
+edit
