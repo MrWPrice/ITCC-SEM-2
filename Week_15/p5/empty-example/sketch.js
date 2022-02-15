d15d6794f048e9786f837dca25a216a6ccc69f3b
MrWPrice <76269054+MrWPrice@users.noreply.github.com>
2022-02-15 16:08:43 +0000

    Update sketch.js

--- Week_15/p5/empty-example/sketch.js
+++ Week_15/p5/empty-example/sketch.js
@@ -1,7 +1,34 @@
+let x, y;
+let radius = 100;
+let r,g,b;
+let timer = 10;
+let interval = 60;
+
 function setup() {
-  // put setup code here
+    createCanvas(windowWidth, windowHeight);
+    x = random(windowWidth);
+    y = random(windowHeight);
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  
 }
 
 function draw() {
-  // put drawing code here
+    background(220); // set background to grey
+    noStroke(); // turn stroke off
+    fill(r, g, b); // create a colour fill with out random rgb values
+    ellipse(x, y, radius*2, radius*2); // draw a circle
+      if (frameCount % interval == 0) { // if the frameCount is divisible by the interval, then the interval (in seconds) has passed and we can draw a new circle
+          
+          newCircle();
+        }
+}
+
+function newCircle() { // reset x, y and rgb values 
+  x = random(windowWidth);
+  y = random(windowHeight);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
