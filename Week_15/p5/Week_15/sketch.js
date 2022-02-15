aca3306b328e94a87274db74313b1137373b474b
MrWPrice <76269054+MrWPrice@users.noreply.github.com>
2022-02-15 16:49:51 +0000

    Update sketch.js

--- Week_15/p5/Week_15/sketch.js
+++ Week_15/p5/Week_15/sketch.js
@@ -3,6 +3,7 @@ let radius = 100;
 let r,g,b;
 let timer = 10;
 let interval = 60;
+let score =0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -19,10 +20,16 @@ function draw() {
     noStroke(); // turn stroke off
     fill(r, g, b); // create a colour fill with out random rgb values
     ellipse(x, y, radius*2, radius*2); // draw a circle
-      if (frameCount % interval == 0) { // if the frameCount is divisible by the interval, then the interval (in seconds) has passed and we can draw a new circle
-          
+      if (frameCount % interval == 0 && timer > 0) { // if the frameCount is divisible by the interval, then the interval (in seconds) has passed and we can draw a new circle
+          timer --; // added countdown
           newCircle();
         }
+    
+        textSize(24);
+        textAlign(LEFT, CENTER);
+        text("Score: " + score, 10, 30);
+        text("Countdown: " + timer, 120, 30);
+
 }
 
 function mousePressed() {
@@ -30,6 +37,7 @@ function mousePressed() {
   let d = dist(mouseX, mouseY, x, y); // find the distance between where the mouse is when pressed and our x and y values
          if (d < radius) { // is the distance smaller than the radius of the circe?
             newCircle(); // create a new circle
+            score++;
             console.log("Score!"); // log that we've been successful - we'll update this bit later.
           }
   }
