fdb3159c87e5e0c555d482bcf958def65cb6baca
MrWPrice <76269054+MrWPrice@users.noreply.github.com>
2022-03-08 14:35:09 +0000

    n

--- Audio_Demo/empty-example/sketch.js
+++ Audio_Demo/empty-example/sketch.js
@@ -13,7 +13,7 @@ function draw(){
   fill(255);
   text('tap to start', width/2, 20);
 
-  micLevel = mic.getLevel();
+  micLevel = mic.getLevel()*6;
   let y = height - micLevel * height;
   ellipse(width/2, y, 10, 10);
 }
