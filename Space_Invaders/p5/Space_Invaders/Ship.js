3f6fbe35b2b4dab71ef198fbe2086050207f3fef
mrwprice <wesley2.price@live.uwe.ac.uk>
2022-03-29 16:50:43 +0100

    Update Ship.js

--- Space_Invaders/p5/Space_Invaders/Ship.js
+++ Space_Invaders/p5/Space_Invaders/Ship.js
@@ -2,8 +2,14 @@ function Ship() {
     this.x = width/2; // will place the ship in the centre of the canvas
 
     this.show =function() {
-
+        fill(255)
+        rectMode(CENTER);
         rect(this.x, height-20, 20, 20);
+    }
+
+    this.move = function(dir) {
+        this.x += dir;
+
     }
     
 }
